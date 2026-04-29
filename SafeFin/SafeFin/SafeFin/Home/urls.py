53eb2c372749352d34173fb2fdebe2e4c63b87c6
Karanpreet1304 <129506258+Karanpreet1304@users.noreply.github.com>
2026-04-29 22:34:33 +0530

    Add files via upload
    block code

---
+++ SafeFin/SafeFin/SafeFin/Home/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from Home import views
+
+urlpatterns = [
+    path("",views.home ,name='Home'),
+    path('home/', views.home, name='home'),
+    path('learn/', views.learn, name='learn'),
+    path("contact/",views.contact, name="contact"),
+    path("calculator/",views.calculator, name="calculator"),
+    path("scams/",views.scams, name="scams"),
+    path("sip_calculator/",views.sip_calculator, name="sip_calculator"),
+    path("mf_calculator/",views.mf_calculator, name="mf_calculator"),
+    path("ppf_calculator/",views.ppf_calculator, name="ppf_calculator"),
+    path("swp_calculator/",views.swp_calculator, name="swp_calculator"),
+    path("search/", views.search, name="search"),
+
+]
